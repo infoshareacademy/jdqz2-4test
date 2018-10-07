8c3b6a0cc8221dc2a413595b7956c298f93c9238
Piotr Domzalski <piotros@poczta.fm>
2018-10-07 19:57:20 +0200

    MainPage update

--- _4test/src/main/java/com/jdqz2/_4test/pageObjects/MainPage.java
+++ _4test/src/main/java/com/jdqz2/_4test/pageObjects/MainPage.java
@@ -1,6 +1,14 @@
 package com.jdqz2._4test.pageObjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MainPage {
+
+    @FindBy(xpath = "//button[@class = 'btn-default searchButton']")
+    private WebElement searchButton;
 
 
 }
+
+
