1d4560c5b12f551346c4c73c4f39f54792d6e34a
kubak <kubakolad@gmail.com>
2018-10-26 17:51:06 +0200

    Added new branch for JDQZTES-80

---
+++ _4test/src/test/java/com/jdqz2/_4test/LoginTest.java
@@ -0,0 +1,19 @@
+package com.jdqz2._4test;
+
+import com.jdqz2._4test.dataGenerator.RegistrationDataGenerator;
+import com.jdqz2._4test.dataGenerator.RegistrationFormData;
+import com.jdqz2._4test.pageObjects.RegistrationPage;
+import com.jdqz2._4test.utils.driver.WebDriverCreators;
+import com.jdqz2._4test.utils.driver.WebDriverProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginTest {
+
+
+
+
+}
